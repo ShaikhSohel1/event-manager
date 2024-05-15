4d46f84c5d3a5065029221b9bc294ea314cfa732
Sohel Shaikh <Sohel.Shaikh@harbingergroup.com>
2024-05-15 17:23:42 +0530

    started clerk auth

--- components/Shared/Header.tsx
+++ components/Shared/Header.tsx
@@ -1,11 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
     <header className='w-full border-b '>
 
-        <div className='wrapper'>
+        <div className='wrapper flex items-center justify-between'>
+        <Link href="/" className='w-36'>
+            <h1 className='font-bold text-purple-950/80'>Events</h1>
+        </Link>
 
+        <div className='flex w-32 justify-end gap-3'>
+        
+        </div>
         </div>
     </header>
   )
